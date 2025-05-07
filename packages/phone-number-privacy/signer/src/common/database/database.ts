a2710a2e86c81560555af1f2accd34df3590280d
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2025-05-07 16:18:38 +0200

    allow selfsigned

--- packages/phone-number-privacy/signer/src/common/database/database.ts
+++ packages/phone-number-privacy/signer/src/common/database/database.ts
@@ -1,30 +1,39 @@
-import { rootLogger } from '@planq-network/phone-number-privacy-common'
-import { Knex, knex } from 'knex'
-import { DEV_MODE, SignerConfig, SupportedDatabase, VERBOSE_DB_LOGGING } from '../../config'
+import {rootLogger} from "@planq-network/phone-number-privacy-common";
+import {Knex, knex} from "knex";
+import {
+  DEV_MODE,
+  SignerConfig,
+  SupportedDatabase,
+  VERBOSE_DB_LOGGING,
+} from "../../config";
 
-export async function initDatabase(config: SignerConfig, migrationsPath?: string): Promise<Knex> {
-  const logger = rootLogger(config.serviceName)
+export async function initDatabase(
+  config: SignerConfig,
+  migrationsPath?: string
+): Promise<Knex> {
+  const logger = rootLogger(config.serviceName);
 
-  logger.info({ config: config.db }, 'Initializing database connection')
-  const { type, host, port, user, password, database, ssl, poolMaxSize } = config.db
+  logger.info({config: config.db}, "Initializing database connection");
+  const {type, host, port, user, password, database, ssl, poolMaxSize} =
+    config.db;
 
-  let connection: any
-  let client: string
+  let connection: any;
+  let client: string;
   if (type === SupportedDatabase.Postgres) {
-    logger.info('Using Postgres')
-    client = 'pg'
+    logger.info("Using Postgres");
+    client = "pg";
     connection = {
       user,
       password,
       database,
       host,
       port: port ?? 5432,
-      ssl,
-      pool: { max: poolMaxSize },
-    }
+      ssl: {rejectUnauthorized: false, require: ssl},
+      pool: {max: poolMaxSize},
+    };
   } else if (type === SupportedDatabase.MySql) {
-    logger.info('Using MySql')
-    client = 'mysql2'
+    logger.info("Using MySql");
+    client = "mysql2";
     connection = {
       user,
       password,
@@ -32,25 +41,25 @@ export async function initDatabase(config: SignerConfig, migrationsPath?: string
       host,
       port: port ?? 3306,
       ssl,
-      pool: { max: poolMaxSize },
-    }
+      pool: {max: poolMaxSize},
+    };
   } else if (type === SupportedDatabase.MsSql) {
-    logger.info('Using MS SQL')
-    client = 'mssql'
+    logger.info("Using MS SQL");
+    client = "mssql";
     connection = {
       user,
       password,
       database,
       server: host,
       port: port ?? 1433,
-      pool: { max: poolMaxSize },
-    }
+      pool: {max: poolMaxSize},
+    };
   } else if (type === SupportedDatabase.Sqlite) {
-    logger.info('Using SQLite')
-    client = 'sqlite3'
-    connection = ':memory:'
+    logger.info("Using SQLite");
+    client = "sqlite3";
+    connection = ":memory:";
   } else {
-    throw new Error(`Unsupported database type: ${type}`)
+    throw new Error(`Unsupported database type: ${type}`);
   }
 
   const db = knex({
@@ -58,15 +67,15 @@ export async function initDatabase(config: SignerConfig, migrationsPath?: string
     useNullAsDefault: type === SupportedDatabase.Sqlite,
     connection,
     debug: DEV_MODE && VERBOSE_DB_LOGGING,
-  })
+  });
 
-  logger.info('Running Migrations')
+  logger.info("Running Migrations");
 
   await db.migrate.latest({
-    directory: migrationsPath ?? './dist/common/database/migrations',
-    loadExtensions: ['.js'],
-  })
+    directory: migrationsPath ?? "./dist/common/database/migrations",
+    loadExtensions: [".js"],
+  });
 
-  logger.info('Database initialized successfully')
-  return db
+  logger.info("Database initialized successfully");
+  return db;
 }
